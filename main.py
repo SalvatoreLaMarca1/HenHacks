b62362fe4145df9780e345fc925a58259c51fc6c
SalvatoreLaMarca1 <148724040+SalvatoreLaMarca1@users.noreply.github.com>
2025-03-01 16:18:16 -0500

    Pills

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,18 +7,42 @@ from db import *
 
 def main():
     
+    user = ""
+    
+    typeOfEvent = st.pills(
+        "What type of event is happening?",
+        ("FIRE", "DISEASE", "CRIME"),
+    )
+    
+    sizeOfEvent = st.pills(
+        "What is the size of the event?",
+        ("SMALL", "MEDIUM", "LARGE")
+    )
+    
+    st.write("Type:", typeOfEvent)
+    st.write("Size: ", sizeOfEvent)
+    
+    now = datetime.now()
+    
+    st.write(now)
+    
+    
     # Define a mapping from type to hex color
     color_mapping = {
-    "FIRE": color_fire_center,
-    "DISEASE": color_disease_center,
-    "CRIME": color_disease_center
+        "FIRE": color_fire_center,
+        "DISEASE": color_disease_center,
+        "CRIME": color_disease_center
     }
-    
-    st.button("Report")
-    st.write("Button clicked!")
     
     df_user = getLocation();
     df_reports = getReports();
+    
+    #st.button("Report", on_click=addReport(getReports(), typeOfEvent, user, df_user[0]["latitude"], df_user[0]["longitude"], sizeOfEvent, now, False, False))
+    
+    
+    st.write("Button clicked!")
+    
+    
     df_reports["color"] = df_reports["type"].map(color_mapping).fillna(color_unknown)
     
     df_points = pd.concat([df_user, df_reports], join='inner')
@@ -26,6 +51,8 @@ def main():
     # current only shows the user
     st.map(df_points, latitude="latitude", longitude="longitude", size="accuracy", color="color")
 
+
+    
     
     
 if __name__ == "__main__":
