30d4d5c2777eafb46955512c27d7aee89265a411
myurkunas <68198232+myurkunas@users.noreply.github.com>
2025-03-01 16:09:07 -0500

    added area mapping of dots

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from db import *
 def main():
     
     # Define a mapping from type to hex color
-    color_mapping = {
+    color_center_mapping = {
     "FIRE": color_fire_center,
     "DISEASE": color_disease_center,
-    "CRIME": color_disease_center
+    "CRIME": color_crime_center
+    }
+    
+    color_area_mapping = {
+    "FIRE": color_fire_area,
+    "DISEASE": color_disease_area,
+    "CRIME": color_crime_area
     }
     
     st.button("Report")
@@ -18,9 +24,15 @@ def main():
     
     df_user = getLocation();
     df_reports = getReports();
-    df_reports["color"] = df_reports["type"].map(color_mapping).fillna(color_unknown)
+    df_center_reports = df_reports.copy(deep=True)
+    df_area_reports = df_reports.copy(deep=True)
+    df_center_reports['accuracy'] = 5
+    df_center_reports["color"] = df_center_reports["type"].map(color_center_mapping).fillna(color_unknown)
+    df_area_reports["color"] = df_area_reports["type"].map(color_area_mapping).fillna(color_unknown)
     
-    df_points = pd.concat([df_user, df_reports], join='inner')
+    df_points = pd.concat([df_user, df_center_reports], join='inner')
+    df_points = pd.concat([df_points, df_area_reports], join='inner')
+    
     print(df_points)
        
     # current only shows the user
